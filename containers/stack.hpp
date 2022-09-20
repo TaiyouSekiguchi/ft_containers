de9ac258061bf391faad7b6e4c7c0b2379cee443
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-20 13:55:17 +0900

    Member function等追加。

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -9,14 +9,49 @@ namespace ft
 	class stack
 	{
 		public:
-			typedef Countainer container_type;
-			typedef typename Container::value_type value_type;
-			typedef typename Container::size_type size_type;
-			typedef typename Container::reference reference;
-			typedef typename Container::const_reference const_reference;
 
+			//Member types
+			typedef Countainer								container_type;
+			typedef typename Container::value_type			value_type;
+			typedef typename Container::size_type			size_type;
+			typedef typename Container::reference			reference;
+			typedef typename Container::const_reference		const_reference;
 
+			// Member function
+			// constructor
+			stack() : c() {}
+			stack(const stack& other) : c(other.c) {}
+			// destructor
+			~stack();
+			// operator
+			stack &operator=(const stack &other) { c = other.c; return *this; }
+			// Element access
+			reference top() { return c.back(); }
+			const_reference top() const { return c.back(); }
+			// Capacity
+			bool empty() const { return c.empty(); }
+			size_type size() const { return c.size(); }
+			// Modifiers
+			void push(const value_type& value) { c.push_back(value); }
+			void pop() { c.pop_back(); }
+
+		protected:
+			container_type c;
 
 	}; // class stack
+
+	// Non-member functions
+	template <class T, class Container>
+	bool operator==(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return x.c == y.c; }
+	template <class T, class Container>
+	bool operator!=(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return !(x == y); }
+	template <class T, class Container>
+	bool operator<(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return x.c < y.c; }
+	template <class T, class Container>
+	bool operator>(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return x.c > y.c; }
+	template <class T, class Container>
+	bool operator>=(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return !(x < y); }
+	template <class T, class Container>
+	bool operator<=(const ft::stack<T, Container> &x, const ft::stack<T, Container> &y) { return !(x > y); }
 
 } // namespace ft
