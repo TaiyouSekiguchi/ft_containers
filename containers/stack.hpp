068810b6487d5bf8a7a5c2261f197da5e07afde2
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-20 12:58:11 +0900

    member type 追加。

--- containers/stack.hpp
+++ containers/stack.hpp
@@ -8,7 +8,12 @@ namespace ft
 	template <class T, class Container = ft::vector<T> >
 	class stack
 	{
-
+		public:
+			typedef Countainer container_type;
+			typedef typename Container::value_type value_type;
+			typedef typename Container::size_type size_type;
+			typedef typename Container::reference reference;
+			typedef typename Container::const_reference const_reference;
 
 
 
