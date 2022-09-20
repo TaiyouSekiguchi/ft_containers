1c396394330bf81bc306f6cd216de0c710be8f41
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-09-20 12:44:31 +0900

    雛形作成。

---
+++ containers/stack.hpp
@@ -0,0 +1,17 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+namespace ft
+{
+	template <class T, class Container = ft::vector<T> >
+	class stack
+	{
+
+
+
+
+	}; // class stack
+
+} // namespace ft
