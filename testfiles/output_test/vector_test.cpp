6ad61f48156ca8db3e90e6c087deb8722ecca691
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-10-03 15:58:02 +0900

    constructor_test追加。

--- testfiles/output_test/vector_test.cpp
+++ testfiles/output_test/vector_test.cpp
@@ -1,16 +1,42 @@
 #include "test.hpp"
 
-static void vector_constructor_test()
+static void test_put(const PRE::vector<Number>& v)
 {
-	std::cout << "[ vector_constructor_test ]" << std::endl;
+	cout << "empty()  : " << v.empty() << endl;
+	cout << "size()   : " << v.size() << endl;
+	cout << "content  : [ ";
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << "]" << endl;
+}
 
-	PRE::vector<int> v;
+static void constructor_test()
+{
+	cout << "*********** constructor ***********\n" << endl;
 
-	std::cout << "empty()  : " << v.empty() << std::endl;
-	std::cout << "size()   : " << v.size() << std::endl;
+	cout << "[ default constructor ]" << endl;
+	PRE::vector<Number> v1;
+	test_put(v1);
+
+	cout << "\n[ argument constructor ]" << endl;
+	PRE::vector<Number> v2(3, Number(42));
+	test_put(v2);
+
+	cout << "\n[ iterator constructor ]" << endl;
+	PRE::vector<Number> v3(v2.begin(), v2.end());
+	test_put(v3);
+
+	cout << "\n[ copy constructor ]" << endl;
+	PRE::vector<Number> v4(v2);
+	test_put(v4);
+
+	cout << "\n***********************************\n\n" << endl;
 }
 
 void vector_test()
 {
-	vector_constructor_test();
+	constructor_test();
+
 }
